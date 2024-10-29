77b3bc91b40d77b098d95ec34d961963956719b8
Jonathan <jonathanrubens10@gmail.com>
2024-10-29 13:35:53 -0300

    Segundo Commit

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyCf3NIHIgdZ_pOFnzyjG5UkePwL3eS8Dgc",
+    authDomain: "codeacademy-6aff5.firebaseapp.com",
+    databaseURL: "https://codeacademy-6aff5-default-rtdb.firebaseio.com",
+    projectId: "codeacademy-6aff5",
+    storageBucket: "codeacademy-6aff5.appspot.com",
+    messagingSenderId: "821757995454",
+    appId: "1:821757995454:web:6699df526a779aaaebf1ef"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
